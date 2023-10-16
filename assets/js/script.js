1aba0fc50626084e788f6a679f5f777c75f348c2
afontana405 <alex13579@icloud.com>
2023-10-16 03:07:08 -0500

    when search button is clicked, weather info will be dispalyed

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,8 +6,8 @@ $(document).ready(function() {
     var currentTempEl = $('#currentTemp');
     var currentHumidityEl = $('#currentHumidity');
     var currentIconEl = $('#currentIcon');
-    var forecastEl = $('#forecastWeather');
     var cityEl = $('#cityName');
+    var searchHistoryBtn = $('.searchHistoryBtn');
     var forecastDaySelector = [0,8,16,24,32];
 
     // on btn click, runs find cords function
@@ -34,6 +34,7 @@ $(document).ready(function() {
     function recentsearches() {
         var city = inputEl.val();
         var lastSearchEl = document.createElement('button');
+        lastSearchEl.className = 'searchHistoryBtn';
         lastSearchEl.textContent = city;
         searchHistory.append(lastSearchEl);
     }
@@ -47,7 +48,7 @@ $(document).ready(function() {
             .then(function (data){
                 displayWeather(data)
             })
-    }
+        }
     // displays weather info pulled from API
     function displayWeather(data) {
         var city = inputEl.val();
@@ -61,7 +62,7 @@ $(document).ready(function() {
     function getForecast(latitude, longitude) {
         ForecastUrl = 'https://api.openweathermap.org/data/2.5/forecast?lat=' + latitude + '&lon=' + longitude +  '&cnt=33&units=imperial&appid=7ff589e4bc4b4b8d6fc8df8bb1158396'
         fetch(ForecastUrl)
-            .then(function (response){
+        .then(function (response){
                 return response.json()
             })
             .then(function (data) {
@@ -76,5 +77,45 @@ $(document).ready(function() {
             console.log(forecastDisplayEl);
             forecastDisplayEl.textContent = data.list[forecastDaySelector[i]].dt_txt + '\ntemp: ' + data.list[forecastDaySelector[i]].main.temp + '°F' + 'Wind Speed: ' + data.list[forecastDaySelector[i]].wind.speed + 'mph' + '\nHumidity: ' + data.list[forecastDaySelector[i]].main.humidity + '%';
         }
+    }
+    // calls function when search history button is clicked
+    $(document).on("click", searchHistoryBtn, function () {
+        var cityName = event.target.textContent;
+        if (cityName) {
+            refindWeather(cityName);
+        }
+    });
+    // finds weather after search button is clicked
+    function refindWeather(cityName) {
+        geoCoderUrl = 'http://api.openweathermap.org/geo/1.0/direct?q=' + cityName + '&limit=1&appid=7ff589e4bc4b4b8d6fc8df8bb1158396'
+        fetch(geoCoderUrl)
+        .then(function (response) {
+            return response.json();
+        })
+        .then(function (data) {
+            var longitude = data[0].lon;
+            var latitude = data[0].lat;
+            reGetWeather(cityName, latitude, longitude);
+            getForecast(latitude, longitude);
+        });
+    }
+    // gets weather info after search button is clicked
+    function reGetWeather(cityName, latitude, longitude) {
+        weatherUrl = 'https://api.openweathermap.org/data/2.5/weather?lat=' + latitude + '&lon=' + longitude +  '&units=imperial&appid=7ff589e4bc4b4b8d6fc8df8bb1158396'
+        fetch(weatherUrl)
+            .then(function (response) {
+                return response.json()
+            })
+            .then(function (data){
+                reDisplayWeather(cityName, data)
+            })
+    }
+    // displays weather info after search button is clicked
+    function reDisplayWeather(cityName, data) {
+        cityEl.text(cityName);
+        currentWindEl.text("wind speed: " + data.wind.speed + "mph");
+        currentTempEl.text("temp: " + data.main.temp + "°F");
+        currentHumidityEl.text("humidity: " + data.main.humidity + "%");
+        currentIconEl.text(data.weather[0].icon);
     }
 });
